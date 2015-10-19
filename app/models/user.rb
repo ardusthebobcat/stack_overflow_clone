d30089a13d288a6895ea2a9012181bd19f61e237
Justin Scott & Michael Summers <student@epicodus.com>
2015-10-19 09:53:13 -0700

    Add factory for user and full_name method for user

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ActiveRecord::Base
   has_many :answers
 
   validates :first_name, :last_name, :email, :password_hash, presence: true
+
+  def full_name
+    self.first_name.concat(" ").concat(self.last_name)
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -7,4 +7,9 @@ describe User do
   it {should validate_presence_of :password_hash}
   it {should have_many :posts}
   it {should have_many :answers}
+
+  it "returns the full name of the user" do
+    user = FactoryGirl.create(:user)
+    expect(user.full_name).to eq "John Smith"
+  end
 end
