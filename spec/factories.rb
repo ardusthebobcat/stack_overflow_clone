5f7b1965ddf3f3393c585638f71739718e9d400e
Justin Scott & Michael Summers <student@epicodus.com>
2015-10-19 15:57:29 -0700

    Add post pages integration specs

--- spec/factories.rb
+++ spec/factories.rb
@@ -4,5 +4,12 @@ FactoryGirl.define do
     last_name("Smith")
     email("fake@fake.com")
     password("fakepassword")
+    id(1)
+  end
+
+  factory(:post) do
+    question("Test")
+    description("Test")
+    user_id(1)
   end
 end
---
+++ spec/features/post_pages_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+describe "add new post" do
+  it "will add a new post" do
+    user = FactoryGirl.create(:user)
+    visit '/'
+    click_on "LOG-IN"
+    fill_in "Email", with: "fake@fake.com"
+    fill_in "Password", with: "fakepassword"
+    click_on "Log In"
+    click_on "Add Post"
+    fill_in "Question", with: "Test"
+    fill_in "Description", with: "Test"
+    click_on "Create Post"
+    expect(page).to have_content "Test"
+  end
+
+  it "will give errors if post isn't created" do
+    user = FactoryGirl.create(:user)
+    visit new_session_path
+    fill_in "Email", with: "fake@fake.com"
+    fill_in "Password", with: "fakepassword"
+    click_on "Log In"
+    click_on "Add Post"
+    click_on "Create Post"
+    expect(page).to have_content "There was a problem"
+  end
+end
+
+describe "post page" do
+  it "will show a specific posts page" do
+    user = FactoryGirl.create(:user)
+    post = FactoryGirl.create(:post)
+    visit'/'
+    click_on "Test"
+    expect(page).to have_content "Test"
+  end
+end
